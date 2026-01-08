cda648f918a8c156893af8bd340f234ee6f20bf6
cpcr <cpcr@homewood-tilt-laptop.local>
2026-01-08 14:02:13 -0500

    ses199

--- tilt_mocs/tilt_mocs_lastrun.py
+++ tilt_mocs/tilt_mocs_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.2.4),
-    on Tue Jan  6 14:35:58 2026
+    on Thu Jan  8 12:24:00 2026
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
